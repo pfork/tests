3c6581dac2105fc29a6e0085d1c3171760856c47
stef <pitchfork@ctrlc.hu>
2017-03-11 23:40:44 +0000

    [mod] added rsp.[ch]

---
+++ rsp.h
@@ -0,0 +1,9 @@
+#ifndef rsp_h
+#define rsp_h
+
+/* functions for pyrsp based testing */
+void rsp_finish(void);
+void rsp_detach(void);
+void rsp_dump(unsigned char* buf, unsigned int buf_size);
+
+#endif // rsp_h
